8ca8b75babb7330fcf17d522f9c3838da411c04e
xuao1 <xuao123@mail.ustc.edu.cn>
2023-11-27 20:48:58 +0800

    update

--- tests/launchKernel.cu
+++ tests/launchKernel.cu
@@ -1,26 +1,42 @@
-#include <cuda_runtime.h>
+#include <stdio.h>
+#include <cuda.h>
+#include <unistd.h>
 #include <iostream>
-#include <unistd.h> 
 
-__global__ void simpleKernel(int *data) {
-    int idx = threadIdx.x;
-    data[idx] = idx;
+__global__ void testKernel(int *a) {
+    int idx = threadIdx.x + blockIdx.x * blockDim.x;
+    a[idx] = a[idx] + 1;
 }
 
 int main() {
-    int *d_data;
-    cudaMalloc(&d_data, sizeof(int) * 256);
+    int *d_a;
+    int size = 256 * sizeof(int);
+    int *a = (int*)malloc(size);
 
-    std::cout << "60 s..." << std::endl;
+    for (int i = 0; i < 256; i++) {
+        a[i] = i;
+    }
+
+    cudaMalloc((void **)&d_a, size);
+
+    cudaMemcpy(d_a, a, size, cudaMemcpyHostToDevice);
+
+    std::cout << "60s..." << std::endl;
     sleep(60);
 
-    simpleKernel<<<1, 256>>>(d_data);
+    testKernel<<<1, 256>>>(d_a);
 
-    std::cout << "60 s..." << std::endl;
+    std::cout << "60s..." << std::endl;
     sleep(60);
 
-    cudaFree(d_data);
+    cudaMemcpy(a, d_a, size, cudaMemcpyDeviceToHost);
 
+    for (int i = 0; i < 10; i++) {
+        printf("%d ", a[i]);
+    }
+    printf("\n");
+
+    cudaFree(d_a);
+    free(a);
     return 0;
 }
-
