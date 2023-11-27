83ab619d0475a1ae1da8388e612fc01a65ec2713
xuao1 <xuao123@mail.ustc.edu.cn>
2023-11-27 20:10:43 +0800

    add log

--- src/nvidia/arch/nvalloc/unix/src/escape.c
+++ src/nvidia/arch/nvalloc/unix/src/escape.c
@@ -294,6 +294,8 @@ NV_STATUS RmIoctl(
     NvU32        dataSize
 )
 {
+    NV_PRINTF(LEVEL_ERROR, "xuao 7. RmIoctl in escape.c\n");
+
     NV_STATUS            rmStatus = NV_ERR_GENERIC;
     API_SECURITY_INFO    secInfo = { };
 
@@ -309,6 +311,7 @@ NV_STATUS RmIoctl(
     {
         case NV_ESC_RM_ALLOC_MEMORY:
         {
+            NV_PRINTF(LEVEL_ERROR, "xuao 8. NV_ESC_RM_ALLOC_MEMORY\n");
             nv_ioctl_nvos02_parameters_with_fd *pApi;
             NVOS02_PARAMETERS *pParms;
 
@@ -360,6 +363,7 @@ NV_STATUS RmIoctl(
 
         case NV_ESC_RM_ALLOC_OBJECT:
         {
+            NV_PRINTF(LEVEL_ERROR, "xuao 8. NV_ESC_RM_ALLOC_OBJECT\n");
             NVOS05_PARAMETERS *pApi = data;
 
             NV_CTL_DEVICE_ONLY(nv);
@@ -376,6 +380,7 @@ NV_STATUS RmIoctl(
 
         case NV_ESC_RM_ALLOC:
         {
+            NV_PRINTF(LEVEL_ERROR, "xuao 8. NV_ESC_RM_ALLOC\n");
             NVOS21_PARAMETERS *pApi = data;
             NVOS64_PARAMETERS *pApiAccess = data;
             NvBool bAccessApi = (dataSize == sizeof(NVOS64_PARAMETERS));
@@ -427,6 +432,7 @@ NV_STATUS RmIoctl(
 
         case NV_ESC_RM_FREE:
         {
+            NV_PRINTF(LEVEL_ERROR, "xuao 8. NV_ESC_RM_FREE\n");
             NVOS00_PARAMETERS *pApi = data;
 
             NV_CTL_DEVICE_ONLY(nv);
@@ -450,6 +456,7 @@ NV_STATUS RmIoctl(
 
         case NV_ESC_RM_VID_HEAP_CONTROL:
         {
+            NV_PRINTF(LEVEL_ERROR, "xuao 8. NV_ESC_RM_VID_HEAP_CONTROL\n");
             NVOS32_PARAMETERS *pApi = data;
 
             NV_CTL_DEVICE_ONLY(nv);
@@ -470,6 +477,7 @@ NV_STATUS RmIoctl(
 
         case NV_ESC_RM_I2C_ACCESS:
         {
+            NV_PRINTF(LEVEL_ERROR, "xuao 8. NV_ESC_RM_I2C_ACCESS\n");
             NVOS_I2C_ACCESS_PARAMS *pApi = data;
 
             NV_ACTUAL_DEVICE_ONLY(nv);
@@ -486,6 +494,7 @@ NV_STATUS RmIoctl(
 
         case NV_ESC_RM_IDLE_CHANNELS:
         {
+            NV_PRINTF(LEVEL_ERROR, "xuao 8. NV_ESC_RM_IDLE_CHANNELS\n");
             NVOS30_PARAMETERS *pApi = data;
 
             NV_CTL_DEVICE_ONLY(nv);
@@ -502,6 +511,7 @@ NV_STATUS RmIoctl(
 
         case NV_ESC_RM_MAP_MEMORY:
         {
+            NV_PRINTF(LEVEL_ERROR, "xuao 8. NV_ESC_RM_MAP_MEMORY\n");
             nv_ioctl_nvos33_parameters_with_fd *pApi;
             NVOS33_PARAMETERS *pParms;
 
@@ -545,6 +555,7 @@ NV_STATUS RmIoctl(
 
         case NV_ESC_RM_UNMAP_MEMORY:
         {
+            NV_PRINTF(LEVEL_ERROR, "xuao 8. NV_ESC_RM_UNMAP_MEMORY\n");
             NVOS34_PARAMETERS *pApi = data;
 
             NV_CTL_DEVICE_ONLY(nv);
@@ -561,6 +572,7 @@ NV_STATUS RmIoctl(
 
         case NV_ESC_RM_ACCESS_REGISTRY:
         {
+            NV_PRINTF(LEVEL_ERROR, "xuao 8. NV_ESC_RM_ACCESS_REGISTRY\n");
             NVOS38_PARAMETERS *pApi = data;
 
             NV_CTL_DEVICE_ONLY(nv);
@@ -587,6 +599,7 @@ NV_STATUS RmIoctl(
 
         case NV_ESC_RM_ALLOC_CONTEXT_DMA2:
         {
+            NV_PRINTF(LEVEL_ERROR, "xuao 8. NV_ESC_RM_ALLOC_CONTEXT_DMA2\n");
             NVOS39_PARAMETERS *pApi = data;
 
             NV_CTL_DEVICE_ONLY(nv);
@@ -603,6 +616,7 @@ NV_STATUS RmIoctl(
 
         case NV_ESC_RM_BIND_CONTEXT_DMA:
         {
+            NV_PRINTF(LEVEL_ERROR, "xuao 8. NV_ESC_RM_BIND_CONTEXT_DMA\n");
             NVOS49_PARAMETERS *pApi = data;
 
             NV_CTL_DEVICE_ONLY(nv);
@@ -619,6 +633,7 @@ NV_STATUS RmIoctl(
 
         case NV_ESC_RM_MAP_MEMORY_DMA:
         {
+            NV_PRINTF(LEVEL_ERROR, "xuao 8. NV_ESC_RM_MAP_MEMORY_DMA\n");
             NVOS46_PARAMETERS *pApi = data;
 
             NV_CTL_DEVICE_ONLY(nv);
@@ -635,6 +650,7 @@ NV_STATUS RmIoctl(
 
         case NV_ESC_RM_UNMAP_MEMORY_DMA:
         {
+            NV_PRINTF(LEVEL_ERROR, "xuao 8. NV_ESC_RM_UNMAP_MEMORY_DMA\n");
             NVOS47_PARAMETERS *pApi = data;
 
             NV_CTL_DEVICE_ONLY(nv);
@@ -651,6 +667,7 @@ NV_STATUS RmIoctl(
 
         case NV_ESC_RM_DUP_OBJECT:
         {
+            NV_PRINTF(LEVEL_ERROR, "xuao 8. NV_ESC_RM_DUP_OBJECT\n");
             NVOS55_PARAMETERS *pApi = data;
 
             NV_CTL_DEVICE_ONLY(nv);
@@ -667,6 +684,7 @@ NV_STATUS RmIoctl(
 
         case NV_ESC_RM_SHARE:
         {
+            NV_PRINTF(LEVEL_ERROR, "xuao 8. NV_ESC_RM_SHARE\n");
             NVOS57_PARAMETERS *pApi = data;
 
             NV_CTL_DEVICE_ONLY(nv);
@@ -683,6 +701,7 @@ NV_STATUS RmIoctl(
 
         case NV_ESC_ALLOC_OS_EVENT:
         {
+            NV_PRINTF(LEVEL_ERROR, "xuao 8. NV_ESC_ALLOC_OS_EVENT\n");
             nv_ioctl_alloc_os_event_t *pApi = data;
 
             if (dataSize != sizeof(nv_ioctl_alloc_os_event_t))
@@ -699,6 +718,7 @@ NV_STATUS RmIoctl(
 
         case NV_ESC_FREE_OS_EVENT:
         {
+            NV_PRINTF(LEVEL_ERROR, "xuao 8. NV_ESC_FREE_OS_EVENT\n");
             nv_ioctl_free_os_event_t *pApi = data;
 
             if (dataSize != sizeof(nv_ioctl_free_os_event_t))
@@ -713,6 +733,7 @@ NV_STATUS RmIoctl(
 
         case NV_ESC_RM_GET_EVENT_DATA:
         {
+            NV_PRINTF(LEVEL_ERROR, "xuao 8. NV_ESC_RM_GET_EVENT_DATA\n");
             NVOS41_PARAMETERS *pApi = data;
 
             if (dataSize != sizeof(NVOS41_PARAMETERS))
@@ -729,6 +750,7 @@ NV_STATUS RmIoctl(
 
         case NV_ESC_STATUS_CODE:
         {
+            NV_PRINTF(LEVEL_ERROR, "xuao 8. NV_ESC_STATUS_CODE\n");
             nv_state_t *pNv;
             nv_ioctl_status_code_t *pApi = data;
 
@@ -757,6 +779,7 @@ NV_STATUS RmIoctl(
 
         case NV_ESC_RM_CONTROL:
         {
+            NV_PRINTF(LEVEL_ERROR, "xuao 8. NV_ESC_RM_CONTROL\n");
             NVOS54_PARAMETERS *pApi = data;
             void *priv = NULL;
             nv_file_private_t *dev_nvfp = NULL;
@@ -820,6 +843,7 @@ NV_STATUS RmIoctl(
 
         case NV_ESC_RM_UPDATE_DEVICE_MAPPING_INFO:
         {
+            NV_PRINTF(LEVEL_ERROR, "xuao 8. NV_ESC_RM_UPDATE_DEVICE_MAPPING_INFO\n");
             NVOS56_PARAMETERS *pApi = data;
             void *pOldCpuAddress;
             void *pNewCpuAddress;
@@ -845,6 +869,7 @@ NV_STATUS RmIoctl(
 
         case NV_ESC_RM_NVLOG_CTRL:
         {
+            NV_PRINTF(LEVEL_ERROR, "xuao 8. NV_ESC_RM_NVLOG_CTRL\n");
             NV_NVLOG_CTRL_PARAMS *pParams = data;
 
             NV_CTL_DEVICE_ONLY(nv);
@@ -879,6 +904,7 @@ NV_STATUS RmIoctl(
 
         case NV_ESC_REGISTER_FD:
         {
+            NV_PRINTF(LEVEL_ERROR, "xuao 8. NV_ESC_REGISTER_FD\n");
             nv_ioctl_register_fd_t *params = data;
             void *priv = NULL;
             nv_file_private_t *ctl_nvfp;
@@ -964,6 +990,7 @@ NV_STATUS RmIoctl(
 
         default:
         {
+            NV_PRINTF(LEVEL_ERROR, "xuao 8. default\n");
             NV_PRINTF(LEVEL_ERROR, "unknown NVRM ioctl command: 0x%x\n", cmd);
             goto done;
         }
